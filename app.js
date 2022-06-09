ceba4ec46ac62267392d6dc95c47771443d44a66
aprylle-1 <aprylle.ablaza@gmail.com>
2022-06-09 14:20:29 +0800

    object enhancements solution

--- app.js
+++ app.js
@@ -138,3 +138,75 @@ const update = (obj, key, val) => {
     newObj[key] = val;
     return newObj;
 }
+
+/*
+---------OBJECT ENHANCEMENTS EXERCISES-----------------
+*/
+
+// function createInstructor(firstName, lastName){
+//     return {
+//       firstName: firstName,
+//       lastName: lastName
+//     }
+//   }
+
+/*
+------------------SAME KEYS AND VALUES ES2015-------------------------
+*/
+const createInstructor = function(firstName, lastName){
+    return{
+        firstName,
+        lastName
+    }
+}
+
+// var favoriteNumber = 42;
+
+// var instructor = {
+//   firstName: "Colt"
+// }
+
+// instructor[favoriteNumber] = "That is my favorite!"
+
+/*
+------------------COMPUTED PROPERTY NAME-------------------------
+*/
+
+let favoriteNumber = 42;
+const instructor = {
+    firstName: 'Colt',
+    [favoriteNumber] : 'That is my favorite!'
+}
+/*
+------------------OBJECT METHODS-------------------------
+*/
+// var instructor = {
+//     firstName: "Colt",
+//     sayHi: function(){
+//       return "Hi!";
+//     },
+//     sayBye: function(){
+//       return this.firstName + " says bye!";
+//     }
+//   }
+
+const newInstructor = {
+    firstName : 'Colt',
+    sayHI (){
+        return 'Hi!';
+    },
+    sayBye (){
+        return this.firstName + " says bye!";
+    }
+}
+
+/*
+------------------CREATE ANIMAL FUNCTION-------------------------
+*/
+
+const createAnimal = function (species, verb, noise){
+    return {
+        species,
+        [verb] : noise,
+    }
+}
